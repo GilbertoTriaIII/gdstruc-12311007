5d7bf18dbfad38a49f96dbef30bb9769d0be539c
GilbertoTriaIII <gilberto.triaiii@benilde.edu.ph>
2025-02-11 17:38:22 +0800

    Sample code for scan input

---
+++ Sample Project/src/HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // print hello world
+        //System.out.println("Hello World!");
+
+        System.out.println("Enter your name: ");
+
+
+        // scan user input
+        Scanner inputScanner = new Scanner(System.in);
+        String name = inputScanner.nextLine();
+
+        System.out.println("Hello " + name + "!");
+    }
+}
+
