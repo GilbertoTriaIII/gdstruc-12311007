1b20e422ad35c7709e0fd683dc5895d8d4562b89
GilbertoTriaIII <gilberto.triaiii@benilde.edu.ph>
2025-02-11 23:39:20 +0800

    Answer for Quiz 2

---
+++ Module0/src/Module0.java
@@ -0,0 +1,14 @@
+public class Module0 {
+    public static void main(String[] args) {
+        int[] nums = new int[5];
+
+        nums[0] = 14;
+        nums[1] = -5;
+        nums[2] = 23;
+        nums[3] = 19;
+        nums[4] = 1;
+
+        System.out.println("Index 2: " + String.valueOf((nums[2]))); // accessing an element
+    }
+}
+
